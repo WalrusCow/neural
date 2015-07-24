f4370438b7eaa53cb93a5238829f1924812bd715
WalrusCow <willgmcd@gmail.com>
2015-07-24 12:55:27 -0400

    Somehow a decent training score

--- neural_net.py
+++ neural_net.py
@@ -2,13 +2,30 @@ import math
 import random
 from collections import namedtuple
 
-class Perceptron():
+class NeuralNode():
     def __init__(self, num_inputs):
         self.weights = [random.random() - 0.5] * (num_inputs + 1)
+        # Store these values at each node
+        self.activation = None
+        self.input = None
+    def process(self, inputs):
+        self.input = 0
+        for w, i in zip(self.weights, inputs):
+            self.input += w * i
+        try:
+            self.activation = sigmoid(self.input)
+        except:
+            print('What the fuck. The weights are: {}'.format(self.weights))
+            print('wtf and the inputs are {}'.format(inputs))
+            print('wtf net input: {}'.format(self.input))
 
 
 def sigmoid(x):
-    return 1 / (1 + math.exp(-x))
+    try:
+        return 1 / (1 + math.exp(-x))
+    except:
+        if x > 0: return 1
+        else: return 0
 
 
 def sigmoid_prime(x):
@@ -26,92 +43,70 @@ def label_to_output(l):
     return 0 if l == 5 else 1
 
 
-NodeData = namedtuple('NodeData', ['inp', 'act'])
-
-
 class NeuralNetwork():
     def __init__(self, levels, activation_fun, activation_prime):
         ''' The first entry in "levels" is the number of inputs. '''
         self._act = activation_fun
         self._act_prime = activation_prime
         self._levels = []
+
         for num_inputs, num_nodes in zip(levels, levels[1:]):
-            self._levels.append([Perceptron(num_inputs)
+            self._levels.append([NeuralNode(num_inputs)
                                  for _ in range(num_nodes)])
 
     def label(self, inputs):
         ''' Label a set of inputs. '''
-        all_outputs = self._get_outputs(inputs)
-        # Final output is the label result
-        return all_outputs[-1][0].act
-
-    def _get_outputs(self, inputs):
-        # Outputs for every level
-        all_outputs = []
-        inputs = [NodeData(0, i) for i in inputs]
-        all_outputs.append(inputs)
+        # Append 1 to the inputs for the bias
+        inputs = inputs[:]
+        inputs.append(1)
         for level in self._levels:
-            outputs = []
+            new_inputs = []
             for node in level:
-                inp = sum(w * n.act for w, n in zip(node.weights, inputs))
-                outputs.append(NodeData(inp, self._act(inp)))
-            # Also append a bias
-            outputs.append(NodeData(0, -1))
-            all_outputs.append(outputs)
-            inputs = outputs
-        return all_outputs
+                node.process(inputs)
+                new_inputs.append(node.activation)
+            # Append 1 for the bias node
+            new_inputs.append(1)
+            inputs = new_inputs
+        return self._levels[-1][0].activation
 
     def _learn_example(self, inputs, true_out, rate):
         ''' Example inputs, the true output and the learning rate. '''
-        node_outputs = self._get_outputs(inputs)
-        net_out = node_outputs[-1][0].act
+        # Our current estimation for the inputs
+        net_out = self.label(inputs)
 
-        prev_deltas = []
-
-        # Backpropagation
+        # Backpropagation of error: Iterate over levels in reverse
         for ln in range(len(self._levels) - 1, -1, -1):
             level = self._levels[ln]
-            deltas = []
             for i in range(len(level)):
                 # Do the ith node on this level
                 node = level[i]
-                node_data = node_outputs[ln + 1][i]
-
-                # Compute the delta
-                gp = self._act_prime(node_data.inp)
-                if not prev_deltas:
+                if ln == len(self._levels) - 1:
                     # We are an output node, so we need to compute our delta
                     # a little differently
-                    delta = gp * (true_out - net_out)
-                    #print('')
-                    #print('')
-                    #print('')
-                    #print('#'*80)
-                    #print('#'*80)
-                    #print('gp: {} true: {} ours: {}'.format(gp,true_out, net_out))
-                    #print('Got the delta as the first node: {}'.format(delta))
-                    #print('#'*80)
-                    #print('#'*80)
-                    #print('')
-                    #print('')
-                    #print('')
+                    node.delta = (true_out - net_out)
                 else:
                     # Get the weight to each child of this node
-                    ww = (n.weights[i] for n in self._levels[ln + 1])
-                    delta = gp * (sum(w * d for w, d in zip(ww, prev_deltas)))
-                    #if (gp < 1e-10):
-                        #print('SMALL GP {} on {}'.format(gp, node_data.inp))
-                    #print('\n\nGot the delta hidden node: {} {} {}'.format(delta, gp, prev_deltas))
+                    d = (n.weights[i] * n.delta for n in self._levels[ln + 1])
+                    node.delta = sum(d)
 
-                # Now that we have our delta...
-                for pi, parent in enumerate(node_outputs[ln]):
-                    # Update the weights for each parent according to how
-                    # much they contributed towards the error (i.e. "delta")
-                    #print('delta: {} act:{}'.format(delta, parent.act))
-                    #print('Updating node weight {} w/ {}'.format(pi, rate*delta*parent.act))
-                    node.weights[pi] += rate * delta * parent.act
-                deltas.append(delta)
-            prev_deltas = deltas
+        # Go forward again to update the weights
+        for ln in range(len(self._levels)):
+            level = self._levels[ln]
+            for node in level:
+                # Get the derivative of our activation function
+                gp = self._act_prime(node.input)
+                if ln > 0:
+                    for pi, parent in enumerate(self._levels[ln - 1]):
+                        #node.weights[pi] += gp * rate * node.delta * parent.activation
+                        node.weights[pi] +=  rate * node.delta * parent.activation
+                else:
+                    # Top level of hidden nodes
+                    for pi, act in enumerate(inputs):
+                        #node.weights[pi] += gp * rate * node.delta * act
+                        node.weights[pi] +=  rate * node.delta * act
+                # Account for bias in every node
+                #node.weights[-1] += gp * rate * node.delta * 1
+                node.weights[-1] +=  rate * node.delta * 1
 
     def learn(self, examples, rate, num_passes=1000):
         ''' Learn from examples. '''
@@ -141,15 +136,17 @@ def score(net, data):
 def main():
     train_data = read_data('trainData.csv', 'trainLabels.csv')
     #train_data = [([0,0],5), ([1,0],5), ([0,1], 5), ([1,1],6)]
-    for lst, _ in train_data:
-        lst.insert(0, -1)
+    #for lst, _ in train_data:
+    #    lst.insert(0, -1)
     num_inputs = len(train_data[0][0])
     #train_data=train_data[:1]
-    NN = NeuralNetwork([num_inputs, 5, 1], sigmoid, sigmoid_prime)
-    NN.learn(train_data, 2.1, 1000)
-    print('Training score: {}'.format(score(NN, train_data)))
+    net = NeuralNetwork([num_inputs, 1, 1], sigmoid, sigmoid_prime)
+    net.learn(train_data, .001, 1000)
+    print('Training score: {}'.format(score(net, train_data)))
 
     test_data = read_data('testData.csv', 'testLabels.csv')
+    print('Testing score: {}'.format(score(net, test_data)))
 
 if __name__ == '__main__':
+
     main()
